1d6f6dcdce60da0459c6974834dd62d76bd2ce2f
sleex89 <samlee723@gmail.com>
2018-06-17 23:39:24 -0700

    Testing server

--- index.js
+++ index.js
@@ -12,10 +12,22 @@ const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'write_products';
 const forwardingAddress = "https://ebaa7fd1.ngrok.io" //Replace this with your HTTPS forwarding address
 
-app.get('/', (req, res) => {
-  res.send("Hello World");
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = `https://${shop}/admin/oath/authorize?client_id=${apiKey}&scope=${scopes}&state=${state}&redirectUri=${redirectUri}`;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+
+  } else {
+    return res.status(400).send('missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+
 })
 
 app.listen(3000, () => {
-
+  console.log('Shopify app listening on port 3000');
 })
