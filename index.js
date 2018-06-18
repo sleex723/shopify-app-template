142d87228b890de79289f27dc2dbcb7db00b4e3a
sleex89 <samlee723@gmail.com>
2018-06-18 00:04:03 -0700

    Getting shop info

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'write_products';
 const forwardingAddress = "https://ebaa7fd1.ngrok.io" //Replace this with your HTTPS forwarding address
 
+//https://ebaa7fd1.ngrok.io/shopify?shop=wunderapps.myshopify.com
+
 app.get('/shopify', (req, res) => {
   const shop = req.query.shop;
   if (shop) {
     const state = nonce();
-    const redirectUri = forwardingAddress + '/shopify/callback';
-    const installUrl = `https://${shop}/admin/oath/authorize?client_id=${apiKey}&scope=${scopes}&state=${state}&redirectUri=${redirectUri}`;
+    const redirectUri = `${forwardingAddress}/shopify/callback`;
+    const installUrl = `https://${shop}/admin/oath/authorize?client_id=${apiKey}&scope=${scopes}&state=${state}&redirect_uri=${redirectUri}`;
 
     res.cookie('state', state);
     res.redirect(installUrl);
@@ -27,6 +29,60 @@ app.get('/shopify', (req, res) => {
   }
 
 })
+
+app.get('/shopify/callback', (req, res) => {
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    const map = Object.assign({}, req.query);
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const generatedHash = crypto
+      .createHmac('sha256', apiSecret)
+      .update(message)
+      .digest('hex');
+
+    if (generatedHash !== hmac) {
+      return res.status(400).send('HMAC Validation Failed');
+    }
+
+    const accessTokenRequestUrl = `https://${shop}/admin/oauth/access_token`;
+    const accessTokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code
+    }
+
+    request.post(accessTokenRequestUrl, {json: accessTokenPayload})
+      .then((accessTokenResponse) => {
+        const accessToken = accessTokenResponse.access_token;
+      
+        const apiRequestUrl = `https://${shop}/admin/shop.json`;
+        const apiRequestHeader = {
+          'X-Shopify-Access-Token': accessToken
+        };
+
+        request.get(apiRequestUrl, { headers: apiRequestHeader })
+          .then(apiResponse => {
+            res.end(apiResponse);
+          })
+          .catch(error => {
+            res.status(error.statusCode).send(error.error.error_description);
+          })
+      })
+      .catch(error => {
+        res.status(error.statusCode).send(error.error.error_description);
+      })
+
+  } else {
+    res.status(400).send('Required parameters missing');
+  }
+});
 
 app.listen(3000, () => {
   console.log('Shopify app listening on port 3000');
